9d0e02da8d509879c379db2dfc3a8b8f6d29a7d6
krkarma777 <krkarma777@gmail.com>
2023-12-06 23:08:53 +0900

    feat: Implement a basic HelloServlet
    - Add HelloServlet class to handle "/hello" URL pattern
    - Extract username parameter from the request
    - Send a simple "hello [username]" response

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,2 +1,27 @@
-package hello.servlet.basic;public class HelloServlet {
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
 }
